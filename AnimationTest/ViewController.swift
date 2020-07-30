7671ea8f3c490b870c8ddd3cfc62d051f1db3785
Vishnu <vishnu@revyrieglobal.com>
2020-07-30 16:30:06 +0530

    Initial Commit

---
+++ AnimationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationTest
+//
+//  Created by macOS on 30/07/20.
+//  Copyright © 2020 macOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
